354d1afc177c65a3f103e3bcf12268ad4999b4df
joe <micminn87@gmail.com>
2023-09-30 20:42:54 +0300

    remove videosdk

--- src/imports.js
+++ src/imports.js
@@ -4,11 +4,11 @@ export let MeetingProvider
 export let useParticipant
 export let usePubSub
 async function Init() {
-  const SDK = await import("@videosdk.live/react-sdk")
-  Constants = SDK.Constants
-  useMeeting = SDK.useMeeting
-  MeetingProvider = SDK.MeetingProvider
-  useParticipant = SDK.useParticipant
-  usePubSub = SDK.usePubSub
+  // const SDK = await import("@videosdk.live/react-sdk")
+  // Constants = SDK.Constants
+  // useMeeting = SDK.useMeeting
+  // MeetingProvider = SDK.MeetingProvider
+  // useParticipant = SDK.useParticipant
+  // usePubSub = SDK.usePubSub
 }
 Init()
